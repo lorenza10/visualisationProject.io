2af09030001e95e7fa3382361b0692b7f4abe7c5
lorenza1010 <lozzenge10@gmail.com>
2020-11-10 23:12:57 +0000

    Refactored piechart.js to reduce from 560 lines of code to under 120

--- piechart.js
+++ piechart.js
@@ -1,6 +1,24 @@
-function updateData() {
+function getIDMen(id) {
 
-    var svg = d3.select("#svgmen"),
+    btn_id = document.getElementById(id).id;
+    string_id = btn_id.replace(/[0-9]/g, '');
+    svg_title = "#svg" + string_id;
+    updateDataMen();
+}
+
+function getIDWomen(id) {
+
+    wbtn_id = document.getElementById(id).id;
+    console.log(wbtn_id);
+    wstring_id = wbtn_id.replace(/[0-9]/g, '');
+    console.log(wstring_id);
+    wsvg_title = "#svg" + wstring_id;
+    updateDataWomen();
+}
+
+function updateDataMen() {
+
+    var svg = d3.select(svg_title),
         width = svg.attr("width"),
         height = svg.attr("height"),
         radius = Math.min(width, height) / 2;
@@ -22,63 +40,7 @@ function updateData() {
         .outerRadius(radius)
         .innerRadius(radius - 80);
 
-    d3.csv("csv/men&women/men/men2012.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function getId(id) {
-
-    btn_id = document.getElementById(id).value;
-    updateData2();
-
-}
-
-
-function updateData2() {
-
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.male;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    var csv_path = "csv/men&women/men/" + btn_id + ".csv"
+    var csv_path = "csv/men&women/" + string_id + "/" + btn_id + ".csv"
 
     d3.csv(csv_path, function(error, data) {
         if (error) {
@@ -104,197 +66,9 @@ function updateData2() {
     });
 }
 
-function updateData3() {
+function updateDataWomen() {
 
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.male;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/men/men2014.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData4() {
-
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.male;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/men/men2015.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData5() {
-
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.male;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/men/men2016.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData6() {
-
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.male;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/men/men2017.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData7() {
-
-    var svg = d3.select("#svgwomen"),
+    var svg = d3.select(wsvg_title),
         width = svg.attr("width"),
         height = svg.attr("height"),
         radius = Math.min(width, height) / 2;
@@ -316,7 +90,9 @@ function updateData7() {
         .outerRadius(radius)
         .innerRadius(radius - 80);
 
-    d3.csv("csv/men&women/women/women2012.csv", function(error, data) {
+    var csv_path = "csv/men&women/" + wstring_id + "/" + wbtn_id + ".csv"
+
+    d3.csv(csv_path, function(error, data) {
         if (error) {
             throw error;
         }
@@ -329,242 +105,6 @@ function updateData7() {
             .attr("d", path)
             .attr("fill", function(d) { return color(d.data.year); });
 
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData8() {
-
-    var svg = d3.select("#svgwomen"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.female;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/women/women2013.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData9() {
-
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.female;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/women/women2014.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData10() {
-
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.female;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/women/women2015.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData11() {
-
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.female;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/women/women2016.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
-
-        console.log(arc)
-
-        arc.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + label.centroid(d) + ")";
-            })
-            .text(function(d) { return d.data.year; });
-    });
-}
-
-function updateData12() {
-
-    var svg = d3.select("svg"),
-        width = svg.attr("width"),
-        height = svg.attr("height"),
-        radius = Math.min(width, height) / 2;
-
-    var g = svg.append("g")
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-    var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c']);
-
-    var pie = d3.pie().value(function(d) {
-        return d.female;
-    });
-
-    var path = d3.arc()
-        .outerRadius(radius - 10)
-        .innerRadius(0);
-
-    var label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 80);
-
-    d3.csv("csv/men&women/women/women2017.csv", function(error, data) {
-        if (error) {
-            throw error;
-        }
-        var arc = g.selectAll(".arc")
-            .data(pie(data))
-            .enter().append("g")
-            .attr("class", "arc");
-
-        arc.append("path")
-            .attr("d", path)
-            .attr("fill", function(d) { return color(d.data.year); });
 
         console.log(arc)
 
