728e358e992d07329976e2b7e9e4f21d0557ccd5
lorenza1010 <lozzenge10@gmail.com>
2020-11-12 15:47:48 +0000

    Made piechart titles dynamic

--- piechart.js
+++ piechart.js
@@ -7,7 +7,14 @@ function getID(id) {
     btn_id = document.getElementById(id).id;
     string_id = btn_id.replace(/[0-9]/g, '');
     svg_title = "#svg" + string_id;
+    chart_title = document.getElementById(id).value
+    clearChart()
     updateData();
+}
+
+function clearChart() {
+    svg = d3.select(svg_title);
+    svg.selectAll("*").remove();
 }
 
 function updateData() {
@@ -29,6 +36,13 @@ function updateData() {
             return d.female
         }
     });
+
+
+    var title = svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", 10)
+        .style("text-anchor", "middle")
+        .text(chart_title);
 
     var path = d3.arc()
         .outerRadius(radius - 10)
@@ -53,13 +67,11 @@ function updateData() {
             .attr("d", path)
             .attr("fill", function(d) { return color(d.data.year); });
 
-
-        console.log(arc)
-
         arc.append("text")
             .attr("transform", function(d) {
                 return "translate(" + label.centroid(d) + ")";
             })
             .text(function(d) { return d.data.year; });
+
     });
 }
